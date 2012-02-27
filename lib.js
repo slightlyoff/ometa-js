b734d9806a509cab7df756c44b2c8f88a7a51541
Alex Russell <slightlyoff@chromium.org>
2012-02-27 14:13:17 +0000

    Clobber StringBuilder, clean up a few globals + ASI issues

--- lib.js
+++ lib.js
@@ -1,61 +1,69 @@
-// try to use StringBuffer instead of string concatenation to improve performance
-
-function StringBuffer() {
-  this.strings = []
-  for (var idx = 0; idx < arguments.length; idx++)
-    this.nextPutAll(arguments[idx])
-}
-StringBuffer.prototype.nextPutAll = function(s) { this.strings.push(s) }
-StringBuffer.prototype.contents   = function()  { return this.strings.join("") }
-String.prototype.writeStream      = function() { return new StringBuffer(this) }
-
 // make Arrays print themselves sensibly
 
-printOn = function(x, ws) {
-  if (x === undefined || x === null)
-    ws.nextPutAll("" + x)
-  else if (x.constructor === Array) {
-    ws.nextPutAll("[")
-    for (var idx = 0; idx < x.length; idx++) {
-      if (idx > 0)
-        ws.nextPutAll(", ")
-      printOn(x[idx], ws)
-    }
-    ws.nextPutAll("]")
-  }
-  else
-    ws.nextPutAll(x.toString())
-}
+Array.prototype.toString = (function() {
 
-Array.prototype.toString = function() { var ws = "".writeStream(); printOn(this, ws); return ws.contents() }
+  var printOn = function(x, s) {
+    s = s || "";
+    if (x === undefined || x === null) {
+      s += ("" + x);
+    } else if (x.constructor === Array) {
+      s += "[";
+      for (var idx = 0; idx < x.length; idx++) {
+        if (idx > 0) {
+          s += ", ";
+        }
+        s = printOn(x[idx], s);
+      }
+      s += "]";
+    } else {
+      s += x;
+    }
+    return s;
+  };
+
+  return function() {
+    return printOn(this);
+  }
+})();
 
 // delegation
 
 objectThatDelegatesTo = function(x, props) {
-  var f = function() { }
-  f.prototype = x
-  var r = new f()
-  for (var p in props)
-    if (props.hasOwnProperty(p))
-      r[p] = props[p]
-  return r
-}
+  var f = function() { };
+  f.prototype = x;
+  var r = new f();
+  for (var p in props) {
+    if (props.hasOwnProperty(p)) {
+      r[p] = props[p];
+    }
+  }
+  return r;
+};
 
 // some reflective stuff
 
 ownPropertyNames = function(x) {
-  var r = []
-  for (var name in x)
-    if (x.hasOwnProperty(name))
-      r.push(name)
+  var r = [];
+  for (var name in x) {
+    if (x.hasOwnProperty(name)) {
+      r.push(name);
+    }
+  }
   return r
-}
+};
 
 isImmutable = function(x) {
-   return x === null || x === undefined || typeof x === "boolean" || typeof x === "number" || typeof x === "string"
-}
+   return (x === null             ||
+           x === undefined        ||
+           typeof x === "boolean" ||
+           typeof x === "number"  ||
+           typeof x === "string"
+          );
+};
 
-String.prototype.digitValue  = function() { return this.charCodeAt(0) - "0".charCodeAt(0) }
+String.prototype.digitValue  = function() {
+  return this.charCodeAt(0) - "0".charCodeAt(0);
+};
 
 isSequenceable = function(x) { return typeof x == "string" || x.constructor === Array }
 
@@ -89,69 +97,82 @@ Array.prototype.delimWith = function(d) {
 // Squeak's ReadStream, kind of
 
 function ReadStream(anArrayOrString) {
-  this.src = anArrayOrString
-  this.pos = 0
+  this.src = anArrayOrString;
+  this.pos = 0;
 }
-ReadStream.prototype.atEnd = function() { return this.pos >= this.src.length }
-ReadStream.prototype.next  = function() { return this.src.at(this.pos++) }
+
+ReadStream.prototype.atEnd = function() {
+  return this.pos >= this.src.length;
+};
+ReadStream.prototype.next  = function() {
+  return this.src.at(this.pos++);
+};
 
 // escape characters
 
 String.prototype.pad = function(s, len) {
-  var r = this
-  while (r.length < len)
-    r = s + r
-  return r
+  var r = this;
+  while (r.length < len) {
+    r = s + r;
+  }
+  return r;
 }
 
-escapeStringFor = new Object()
-for (var c = 0; c < 128; c++)
-  escapeStringFor[c] = String.fromCharCode(c)
-escapeStringFor["'".charCodeAt(0)]  = "\\'"
-escapeStringFor['"'.charCodeAt(0)]  = '\\"'
-escapeStringFor["\\".charCodeAt(0)] = "\\\\"
-escapeStringFor["\b".charCodeAt(0)] = "\\b"
-escapeStringFor["\f".charCodeAt(0)] = "\\f"
-escapeStringFor["\n".charCodeAt(0)] = "\\n"
-escapeStringFor["\r".charCodeAt(0)] = "\\r"
-escapeStringFor["\t".charCodeAt(0)] = "\\t"
-escapeStringFor["\v".charCodeAt(0)] = "\\v"
+escapeStringFor = {};
+for (var c = 0; c < 128; c++) {
+  escapeStringFor[c] = String.fromCharCode(c);
+}
+escapeStringFor["'".charCodeAt(0)]  = "\\'";
+escapeStringFor['"'.charCodeAt(0)]  = '\\"';
+escapeStringFor["\\".charCodeAt(0)] = "\\\\";
+escapeStringFor["\b".charCodeAt(0)] = "\\b";
+escapeStringFor["\f".charCodeAt(0)] = "\\f";
+escapeStringFor["\n".charCodeAt(0)] = "\\n";
+escapeStringFor["\r".charCodeAt(0)] = "\\r";
+escapeStringFor["\t".charCodeAt(0)] = "\\t";
+escapeStringFor["\v".charCodeAt(0)] = "\\v";
+
 escapeChar = function(c) {
   var charCode = c.charCodeAt(0)
-  if (charCode < 128)
-    return escapeStringFor[charCode]
-  else if (128 <= charCode && charCode < 256)
-    return "\\x" + charCode.toString(16).pad("0", 2)
-  else
-    return "\\u" + charCode.toString(16).pad("0", 4)
+  if (charCode < 128) {
+    return escapeStringFor[charCode];
+  } else if (128 <= charCode && charCode < 256) {
+    return "\\x" + charCode.toString(16).pad("0", 2);
+  } else {
+    return "\\u" + charCode.toString(16).pad("0", 4);
+  }
 }
 
+// FIXME(slightlyoff): can we make this private or stuff it on
+//                     String.prototype? I hate that it's global.
 function unescape(s) {
-  if (s.charAt(0) == '\\')
+  if (s.charAt(0) != '\\') {
+    return s;
+  } else {
     switch (s.charAt(1)) {
-      case "'":  return "'"
-      case '"':  return '"'
-      case '\\': return '\\'
-      case 'b':  return '\b'
-      case 'f':  return '\f'
-      case 'n':  return '\n'
-      case 'r':  return '\r'
-      case 't':  return '\t'
-      case 'v':  return '\v'
-      case 'x':  return String.fromCharCode(parseInt(s.substring(2, 4), 16))
-      case 'u':  return String.fromCharCode(parseInt(s.substring(2, 6), 16))
-      default:   return s.charAt(1)
+      case "'":  return "'";
+      case '"':  return '"';
+      case '\\': return '\\';
+      case 'b':  return '\b';
+      case 'f':  return '\f';
+      case 'n':  return '\n';
+      case 'r':  return '\r';
+      case 't':  return '\t';
+      case 'v':  return '\v';
+      case 'x':  return String.fromCharCode(parseInt(s.substring(2, 4), 16));
+      case 'u':  return String.fromCharCode(parseInt(s.substring(2, 6), 16));
+      default:   return s.charAt(1);
     }
-  else
-    return s
+  }
 }
 
 String.prototype.toProgramString = function() {
-  var ws = '"'.writeStream()
-  for (var idx = 0; idx < this.length; idx++)
-    ws.nextPutAll(escapeChar(this.charAt(idx)))
-  ws.nextPutAll('"')
-  return ws.contents()
+  var s = '"';
+  for (var idx = 0; idx < this.length; idx++) {
+    s += escapeChar(this.charAt(idx));
+  }
+  s += '"';
+  return s;
 }
 
 // C-style tempnam function
