f08f7f34881c19456b8a906c113c53a6574bb1ad
Alex Russell <slightlyoff@chromium.org>
2012-03-14 14:26:52 +0000

    Making ometa-script-tag.js readable, dragging it into this decade kicking and screaming

--- ometa-script-tag.js
+++ ometa-script-tag.js
@@ -1,21 +1,29 @@
-function translateCode(s) {
-  var translationError = function(m, i) { alert("Translation error - please tell Alex about this!"); throw fail },
-      tree             = BSOMetaJSParser.matchAll(s, "topLevel", undefined, function(m, i) {
-                                                                              throw objectThatDelegatesTo(fail, {errorPos: i}) })
-  return BSOMetaJSTranslator.match(tree, "trans", undefined, translationError)
-}
-   
-origOnload = window.onload
-window.onload = function() {
-  var scripts = document.getElementsByTagName("script")
-  for (var idx = 0; idx < scripts.length; idx++) {
-    var script = scripts[idx]
-    if (script.type === "text/x-ometa-js")
-      eval(translateCode(script.innerHTML))
-  }
-  if (typeof origOnload === "string")
-    eval(origOnload)
-  else if (typeof origOnload === "function")
-    origOnload()
-}
+(function(scope) {
+ "use strict";
 
+  var translateCode = function(s) {
+    var translationError = function(m, i) {
+      alert("Translation error - please tell Alex about this!");
+      throw fail;
+    };
+    var tree = BSOMetaJSParser.matchAll(
+        s,
+        "topLevel",
+        undefined,
+        function(m, i) {
+          throw om.objectThatDelegatesTo(fail, {errorPos: i});
+        }
+    );
+    return BSOMetaJSTranslator.match(tree, "trans", undefined, translationError);
+  };
+
+  // Old browsers can suck eggs.
+  window.addEventListener("load", function(e) {
+    var scripts = 
+        document.documentElement.querySelectorAll("script[type='text\\/x-ometa-js']");
+    for (var idx = 0; idx < scripts.length; idx++) {
+      eval(translateCode(scripts[idx].innerHTML));
+    }
+  }, false);
+
+})(this);
